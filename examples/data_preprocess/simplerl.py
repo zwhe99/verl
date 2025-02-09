690af071ad04a13efb002d02ff004d09abc6f45a
zwhe99 <hezw.tkcw@gmail.com>
2025-02-09 16:37:59 +0800

    minor

--- examples/data_preprocess/simplerl.py
+++ examples/data_preprocess/simplerl.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     train_dataset = datasets.load_dataset(data_source, split='train')
 
-    def process_fn(example, idx):
+    def process_fn_train(example, idx):
         data = {
             "data_source": data_source,
             "prompt": example['messages'],
@@ -33,5 +33,25 @@ if __name__ == '__main__':
         }
         return data
 
-    train_dataset = train_dataset.map(function=process_fn, with_indices=True)
+    def process_fn_test(example, idx):
+        data = {
+            "data_source": data_source,
+            "prompt": example['messages'],
+            "ability": "math",
+            "reward_model": {
+                "style": "rule",
+                "ground_truth": example['answer']
+            },
+            "extra_info": {
+                'split': 'test',
+                'index': idx,
+                'answer': example['answer'],
+                "question": example['problem'],
+            }
+        }
+        return data
+
+    train_dataset = train_dataset.map(function=process_fn_train, with_indices=True)
+    test_dataset = train_dataset.map(function=process_fn_test, with_indices=True)
     train_dataset.to_parquet(os.path.join(args.local_dir, 'train.parquet'))
+    test_dataset.to_parquet(os.path.join(args.local_dir, 'test.parquet'))
