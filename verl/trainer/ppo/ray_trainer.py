12dc0a73e489b3e85339772b0c97559d1cb2e767
zwhe99 <hezw.tkcw@gmail.com>
2025-03-02 15:54:56 +0800

    turn off val after training if test_freq < 0

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -1089,7 +1089,7 @@ class RayPPOTrainer(object):
                 if self.global_steps >= self.total_training_steps:
 
                     # perform validation after training
-                    if self.val_reward_fn is not None:
+                    if self.val_reward_fn is not None and self.config.trainer.test_freq > 0:
                         val_metrics = self._validate()
                         pprint(f'Final validation metrics: {val_metrics}')
                         logger.log(data=val_metrics, step=self.global_steps)
