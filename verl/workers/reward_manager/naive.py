4ebc8e61bbbd9c4b7852a3882d528b7268f28c15
ttianliang <ttianliang@tencent.com>
2025-07-24 18:56:00 +0800

    debug: print keys

--- verl/workers/reward_manager/naive.py
+++ verl/workers/reward_manager/naive.py
@@ -106,7 +106,7 @@ class NaiveRewardManager:
 
         # get data source list
         data_source_lst = [data[i].non_tensor_batch[self.reward_fn_key] for i in range(len(data))]
-        print(data[i].non_tensor_batch.keys())
+        print(data[0].non_tensor_batch.keys())
         exit(0)
 
         # get prompt str list
