ecebfe96ef96ea8de05078c315134917ca9f07cc
ttianliang <ttianliang@tencent.com>
2025-11-14 17:18:31 +0800

    bug fixed: kimi acc_lst

--- verl/workers/reward_manager/naive.py
+++ verl/workers/reward_manager/naive.py
@@ -177,22 +177,21 @@ class NaiveRewardManager:
         
         if self.config.custom_reward_function.length_reward.ablation.kimi:
             group_size = self.config.actor_rollout_ref.rollout.n
-            group_acc_lst = [acc_lst[i : i + group_size] for i in range(0, len(acc_lst), group_size)]
             group_valid_response_length = [
                 valid_response_length[i : i + group_size] for i in range(0, len(valid_response_length), group_size)
             ]
-            group_kimi_lambda = []
+            kimi_lambda_lst = []
             for gvr_len in group_valid_response_length:
                 max_len = max(gvr_len)
                 min_len = min(gvr_len)
                 if max_len == min_len:
-                    group_kimi_lambda.extend([0] * group_size)
+                    kimi_lambda_lst.extend([0] * group_size)
                 else:
                     for l in gvr_len:
-                        group_kimi_lambda.append(0.5 - ((l - min_len) / (max_len - min_len)))
+                        kimi_lambda_lst.append(0.5 - ((l - min_len) / (max_len - min_len)))
                     
             length_reward = []
-            for _acc, _lambda in zip(group_acc_lst, group_kimi_lambda):
+            for _acc, _lambda in zip(acc_lst, kimi_lambda_lst):
                 if _acc == 1.0:
                     length_reward.append(_lambda)
                 elif _acc == 0.0:
