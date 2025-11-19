5f5ea861db7aac8ca4e9f7da553713744961f577
ttianliang <ttianliang@tencent.com>
2025-11-19 19:38:21 +0800

    x

--- verl/workers/reward_manager/naive.py
+++ verl/workers/reward_manager/naive.py
@@ -106,7 +106,10 @@ class NaiveRewardManager:
 
         # get data source list
         data_source_lst = [data[i].non_tensor_batch[self.reward_fn_key] for i in range(len(data))]
-        print(data[0].non_tensor_batch.keys()) # dict_keys(['data_source', 'ability', 'reward_model', 'extra_info', 'r1_content', 'r1_reasoning_content', 'r1', 'r1_with_ans_label', 'index', 'uid'])
+        if "difficulty" in data[0].non_tensor_batch.keys():
+            data_difficulty_lst = [data[i].non_tensor_batch['difficulty'] for i in range(len(data))]
+        print(data_difficulty_lst)
+        # print(data[0].non_tensor_batch.keys()) # dict_keys(['data_source', 'ability', 'reward_model', 'extra_info', 'r1_content', 'r1_reasoning_content', 'r1', 'r1_with_ans_label', 'index', 'uid'])
         # data_difficulty_lst = [data[i].non_tensor_batch['extra_info']['difficulty'] for i in range(len(data))]
         # data_difficulty_mean = 6.0
 
